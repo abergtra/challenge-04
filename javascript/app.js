5dbbc45232899bbe8bfedd64da94282c84a86089
Asher <asher.bergtraun@gmail.com>
2021-09-27 03:37:45 -0700

    eliminated unnecessary files and code

--- javascript/app.js
+++ javascript/app.js
@@ -147,88 +147,6 @@ function generateQuestion(){
         questionNumber++;
     }
 };
-// function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-// 	function showQuestions(questions, quizContainer){
-//         // we'll need a place to store the output and the answer answer
-//         var output = [];
-//         var answers;
-    
-//         // for each question...
-//         for(var i=0; i<questions.length; i++){
-            
-//             // first reset the list of answers
-//             answers = [];
-    
-//             // for each available answer to this question...
-//             for(letter in questions[i].answers){
-    
-//                 // ...add an html radio button
-//                 answers.push(
-//                     '<label>'
-//                         + '<input type="radio" name="question'+i+'" value="'+letter+'">'
-//                         + letter + ': '
-//                         + questions[i].answers[letter]
-//                     + '</label>'
-//                 );
-//             }
-    
-//             // add this question and its answers to the output
-//             output.push(
-//                 '<div class="question">' + questions[i].question + '</div>'
-//                 + '<div class="answers">' + answers.join('') + '</div>'
-//             );
-//         }
-    
-//         // finally combine our output list into one string of html and put it on the page
-//         quizContainer.innerHTML = output.join('');
-//     }
-
-// 	function showResults(questions, quizContainer, resultsContainer){
-	
-//         // gather answer containers from our quiz
-//         var answerContainers = quizContainer.querySelectorAll('.answers');
-        
-//         // keep track of user's answers
-//         var userAnswer = '';
-//         var numCorrect = 0;
-        
-//         // for each question...
-//         for(var i=0; i<questions.length; i++){
-
-//             // find selected answer
-//             userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-            
-//             // if answer is correct
-//             if(userAnswer===questions[i].correctAnswer){
-//                 // add to the number of correct answers
-//                 numCorrect++;
-                
-//                 // color the answers green
-//                 answerContainers[i].style.color = 'lightgreen';
-//             }
-//             // if answer is wrong or blank
-//             else{
-//                 // color the answers red
-//                 answerContainers[i].style.color = 'red';
-//             }
-//         }
-
-//         // show number of correct answers out of total
-//         resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-//     }
-
-// 	// show the questions
-// 	showQuestions(questions, quizContainer);
-
-// 	// when user clicks submit, show results
-// 	submitButton.onclick = function(){
-// 		showResults(questions, quizContainer, resultsContainer);
-// 	}
-// }
-
-// generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-
 
 // function for start quiz button being pressed
 startButton.onclick = function(){
@@ -245,14 +163,14 @@ function checkAnswer(answer){
         score++;
         alert("Correct!");
         currentQuestionIndex++;
-        generateQuizQuestion();
+        generateQuestion();
         //display in the results div that the answer is correct.
     } else if (answer !== correct && currentQuestionIndex !== finalQuestionIndex) {
         // If incorrect, subtract 10sec from timer
         count = count - penalty;
         alert("Incorrect!")
         currentQuestionIndex++;
-        generateQuizQuestion();
+        generateQuestion();
         //display in the results div that the answer is wrong.
     } else{
         showScore();
